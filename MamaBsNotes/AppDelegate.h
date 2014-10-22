7adf9e83f62053d88996f6ad72ffe7e47fb6c50d
Troy <tgb2110@columbia.edu>
2014-10-22 14:55:32 -0400

    Initial Commit

---
+++ MamaBsNotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MamaBsNotes
+//
+//  Created by OB Troy on 10/21/14.
+//  Copyright (c) 2014 OB Troy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MamaBsNotes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MamaBsNotes
+//
+//  Created by OB Troy on 10/21/14.
+//  Copyright (c) 2014 OB Troy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
